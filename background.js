282d9cd1945cae4c396bd42112503e03241fb2c6
Kazuya Takahashi <electrolysis.jp@gmail.com>
2019-08-07 03:16:47 +0900

    Add menu items

--- background.js
+++ background.js
@@ -1,1 +1,14 @@
 "use strict";
+
+browser.search.get().then(engines => {
+    for (const engine of engines) {
+        browser.menus.create({
+            id: engine.name,
+            title: engine.name,
+            contexts: ["selection"],
+            icons: engine.favIconUrl && {
+                "32": engine.favIconUrl
+            }
+        });
+    }
+});
